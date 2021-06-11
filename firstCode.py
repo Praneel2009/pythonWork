814282655309ec652af1a11f4bd0edcf2d13aaec
praneel <praneelyar2009@gmail.com>
2021-06-11 18:02:07 -0500

    hello

--- firstCode.py
+++ firstCode.py
@@ -1,2 +1,9 @@
-name = input("what is your name? ")
-print("Hello", name )
+name1 = "praneel" #input("what is your name? ")
+name2= "suhas"
+print("My Name Is: ", name1 )
+
+score1 = int(input("score1: "))
+score2 = int(input("score2: "))
+
+
+print (score2 - score1)
