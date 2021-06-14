e32dee65e41976961742e1d81fcb61df3b919ee8
praneel <praneelyar2009@gmail.com>
2021-06-13 21:18:06 -0500

    <Notes>

--- firstCode.py
+++ firstCode.py
@@ -1,9 +1,23 @@
-name1 = "praneel" #input("what is your name? ")
-name2= "suhas"
-print("My Name Is: ", name1 )
+firstName= "Praneel"  # input("whats your name? ")
+lastName = "Yarlagadda"
 
-score1 = int(input("score1: "))
-score2 = int(input("score2: "))
+fullName = firstName + " " + lastName
+score1 = 34 # int(input("score1: "))
+score2 = 56 # int(input("score2: "))
 
+print("My Name is: " + fullName + ", not done yet,....")
+print("Result: " + str(score2-score1) + ", end of program....")
+finalScore=score2 + score1
+print("My Name: %s, my score: %i" % (fullName, finalScore))
+print("My Name: {} my score: {}" .format(finalScore, fullName))
+print("My Name: {first}, my score: {second}".format(second=fullName, first=finalScore))
+print(f"My Name: {fullName}, my score: {finalScore}") # This is the best way so use it like this
 
-print (score2 - score1)
+# 1. plain format, passing multiple values
+# 2. string concatenate : sting join
+# 3. using % format
+# 4. using format()
+# 5. using format() with key-value
+# 6. using f-string
+# 7. using %s means it's a string %s = string
+# 8. using %i means it's an integer %i = integer
