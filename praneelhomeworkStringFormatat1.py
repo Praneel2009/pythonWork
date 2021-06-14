edc05804be7d685d5c6648597b7184c75a3e3be0
praneel <praneelyar2009@gmail.com>
2021-06-13 22:56:55 -0500

    Homework

---
+++ praneelhomeworkStringFormatat1.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+school = input("What is your school? ")
+grade = int(input("What grade are you in? "))
+
+
+
+print(f"{name} goes to {school} and is in {grade}th grade")
+
+
