f799704a318c88b2325d4872503f2269aa883ee0
praneel <praneelyar2009@gmail.com>
2021-06-15 11:01:57 -0500

    stringSlicesHomework.py

--- firstCode.py
+++ firstCode.py
@@ -5,19 +5,40 @@ fullName = firstName + " " + lastName
 score1 = 34 # int(input("score1: "))
 score2 = 56 # int(input("score2: "))
 
-print("My Name is: " + fullName + ", not done yet,....")
-print("Result: " + str(score2-score1) + ", end of program....")
-finalScore=score2 + score1
-print("My Name: %s, my score: %i" % (fullName, finalScore))
-print("My Name: {} my score: {}" .format(finalScore, fullName))
-print("My Name: {first}, my score: {second}".format(second=fullName, first=finalScore))
-print(f"My Name: {fullName}, my score: {finalScore}") # This is the best way so use it like this
-
-# 1. plain format, passing multiple values
+# print("My Name is: " + fullName + ", not done yet,....")
+#print("Result: " + str(score2-score1) + ", end of program....")
+# finalScore=score2 + score1
+# print("My Name: %s, my score: %i" % (fullName, finalScore))
+# print("My Name: {} my score: {}" .format(finalScore, fullName))
+# print("My Name: {first}, my score: {second}".format(second=fullName, first=finalScore))
+# print(f"My Name: {fullName}, my score: {finalScore}") # This is the best way so use it like this
+# plain format, passing multiple values
 # 2. string concatenate : sting join
 # 3. using % format
 # 4. using format()
 # 5. using format() with key-value
 # 6. using f-string
 # 7. using %s means it's a string %s = string
-# 8. using %i means it's an integer %i = integer
+# 8. using %i means it's an integer %i = integer school = "Killian"  #"Harmony"
+school = "killIAn"
+
+print(school[0])
+length = (len(school))
+print(school[0])
+print(school[3:])
+# print(school<start>:<end>:<increment: 1 or -1>)
+print(school[6:1:-1])# print(school[1:6:2])
+print(school[2:6:2])
+print(school[1:6:2])
+
+print(school[0].upper() + school[1:].lower())
+# # 1 = (-1): 0
+# # 0 + (-1): -1
+# name = "prANeEl yarLAgAdDa"
+# print(name.upper(0 + 8))
+#
+#
+# # input: "killian"
+# # output: 1st letter in UPPER. all other in LOWER : Killian
+#
+#
---
+++ stringSlicesHomework.py
@@ -0,0 +1,17 @@
+name ="pRaNEel"
+
+print(name[3])
+print(name[2:6])
+print(name[4:7:2])
+print(name[0].upper() + name[1:7].lower())
+print(name[0:-1].upper() + name[1:7:].lower())
+print(name[::-1])
+
+food = "sPagATti"
+
+print(food[7:2:-1])
+print(food[:])
+print(food[3:8])
+print(food[:].lower())
+print(food[::-1].upper())
+print(food[7:5:-1])
